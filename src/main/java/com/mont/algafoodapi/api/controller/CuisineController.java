7fb0b09e9353758f019a871c85558339a980e72c
Pedro Monteiro <pedromont436@gmail.com>
2023-06-27 14:51:26 -0300

    CuisineController findById method

--- src/main/java/com/mont/algafoodapi/api/controller/CuisineController.java
+++ src/main/java/com/mont/algafoodapi/api/controller/CuisineController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,10 @@ public class CuisineController {
     @GetMapping
     public List<Cuisine> findAll() {
         return cuisineRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Cuisine findById(@PathVariable Long id) {
+        return cuisineRepository.findById(id);
     }
 }
