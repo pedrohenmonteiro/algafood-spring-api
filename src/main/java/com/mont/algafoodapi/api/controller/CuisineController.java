758e881f2d748546b74271962240a4cc5337a6db
Pedro Monteiro <pedromont436@gmail.com>
2023-06-27 21:14:39 -0300

    CuisineController create  method

--- src/main/java/com/mont/algafoodapi/api/controller/CuisineController.java
+++ src/main/java/com/mont/algafoodapi/api/controller/CuisineController.java
@@ -3,8 +3,13 @@ package com.mont.algafoodapi.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,12 +24,17 @@ public class CuisineController {
     private CuisineRepository cuisineRepository;
 
     @GetMapping
-    public List<Cuisine> findAll() {
-        return cuisineRepository.findAll();
+    public ResponseEntity<List<Cuisine>> findAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(cuisineRepository.findAll());
     }
 
     @GetMapping("/{id}")
-    public Cuisine findById(@PathVariable Long id) {
-        return cuisineRepository.findById(id);
+    public ResponseEntity<Cuisine> findById(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(cuisineRepository.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Cuisine> create(@RequestBody Cuisine cuisine) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(cuisineRepository.save(cuisine));
     }
 }
--- src/main/java/com/mont/algafoodapi/model/Cuisine.java
+++ src/main/java/com/mont/algafoodapi/model/Cuisine.java
@@ -1,5 +1,8 @@
 package com.mont.algafoodapi.model;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
