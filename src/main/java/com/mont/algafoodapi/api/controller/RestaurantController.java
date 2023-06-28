d7c5d8d293c9ece627f96c52a7df3640f28ff408
Pedro Monteiro <pedromont436@gmail.com>
2023-06-28 17:18:42 -0300

    RestaurantController findAll method

--- src/main/java/com/mont/algafoodapi/api/controller/RestaurantController.java
+++ src/main/java/com/mont/algafoodapi/api/controller/RestaurantController.java
@@ -1,15 +1,29 @@
 package com.mont.algafoodapi.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mont.algafoodapi.domain.model.Restaurant;
 import com.mont.algafoodapi.domain.service.RestaurantService;
 
 @RestController
-@RequestMapping
+@RequestMapping("/restaurants")
 public class RestaurantController {
     
     @Autowired
     private RestaurantService restaurantService;
+
+    @GetMapping
+    public ResponseEntity<List<Restaurant>> findAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(restaurantService.findAll());
+    }
+
+    
+
 }
