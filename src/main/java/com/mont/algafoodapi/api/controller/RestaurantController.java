7cf83dfda7f2bdbaa8e7ca35551913c26a329159
Pedro Monteiro <pedromont436@gmail.com>
2023-06-28 19:02:40 -0300

    RestaurantService create refactoring

--- src/main/java/com/mont/algafoodapi/api/controller/RestaurantController.java
+++ src/main/java/com/mont/algafoodapi/api/controller/RestaurantController.java
@@ -11,7 +11,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mont.algafoodapi.domain.exception.BadRequestException;
+import com.mont.algafoodapi.domain.exception.NotFoundException;
 import com.mont.algafoodapi.domain.model.Restaurant;
+import com.mont.algafoodapi.domain.repository.CuisineRepository;
 import com.mont.algafoodapi.domain.service.RestaurantService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -24,6 +27,9 @@ public class RestaurantController {
     
     @Autowired
     private RestaurantService restaurantService;
+
+    @Autowired
+    private CuisineRepository cuisineRepository;
 
     @GetMapping
     public ResponseEntity<List<Restaurant>> findAll() {
@@ -38,6 +44,13 @@ public class RestaurantController {
 
     @PostMapping
     public ResponseEntity<Restaurant> create(@RequestBody Restaurant restaurant) {
+        Long cuisineId = restaurant.getCuisine().getId();
+        var cuisine = cuisineRepository.findById(cuisineId);
+        if(cuisine == null) {
+            throw new BadRequestException("Resource cuisine id " + cuisineId + " not found"); 
+        }
+        restaurant.setCuisine(cuisine);
+
         return ResponseEntity.status(HttpStatus.CREATED).body(restaurantService.create(restaurant));
     }
 
