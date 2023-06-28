9eae19fa06ecd67cef1c539265f45eb4c169642a
Pedro Monteiro <pedromont436@gmail.com>
2023-06-28 17:00:54 -0300

    RestaurantService class findAll method

---
+++ src/main/java/com/mont/algafoodapi/domain/service/RestaurantService.java
@@ -0,0 +1,22 @@
+package com.mont.algafoodapi.domain.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mont.algafoodapi.domain.model.Restaurant;
+import com.mont.algafoodapi.domain.repository.RestaurantRepository;
+
+@Service
+public class RestaurantService {
+    
+    @Autowired
+    private RestaurantRepository restaurantRepository;
+
+    public List<Restaurant> findAll() {
+        return restaurantRepository.findAll();
+    }
+
+   
+}
