9adb5ac1ff80a40756d8c6498e6e56885f1191e4
Pedro Monteiro <pedromont436@gmail.com>
2023-06-29 18:04:39 -0300

    Refactoring RestaurantService

--- src/main/java/com/mont/algafoodapi/domain/service/RestaurantService.java
+++ src/main/java/com/mont/algafoodapi/domain/service/RestaurantService.java
@@ -3,40 +3,28 @@ package com.mont.algafoodapi.domain.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
-import com.mont.algafoodapi.domain.exception.BadRequestException;
 import com.mont.algafoodapi.domain.exception.NotFoundException;
 import com.mont.algafoodapi.domain.model.Restaurant;
-import com.mont.algafoodapi.domain.repository.CuisineRepository;
 import com.mont.algafoodapi.domain.repository.RestaurantRepository;
 
 @Service
 public class RestaurantService {
 
-    @Autowired
+    @Autowired 
     private RestaurantRepository restaurantRepository;
-
-    @Autowired
-    private CuisineRepository cuisineRepository;
 
     public List<Restaurant> findAll() {
         return restaurantRepository.findAll();
+        
     }
 
     public Restaurant findById(Long id) {
-        return restaurantRepository.findById(id);
+        return getRestaurant(id);
     }
-
+    
     public Restaurant create(Restaurant restaurant) {
-        Long cuisineId = restaurant.getCuisine().getId();
-        var cuisine = cuisineRepository.findById(cuisineId);
-        if(cuisine == null) {
-            throw new BadRequestException("Resource cuisine id " + cuisineId + " not found"); 
-        }
-        restaurant.setCuisine(cuisine);
         return restaurantRepository.save(restaurant);
     }
 
@@ -46,13 +34,11 @@ public class RestaurantService {
     }
 
     public void delete(Long id) {
-        try {
-        restaurantRepository.delete(id);
-         } catch(DataIntegrityViolationException exception) {
-            throw new BadRequestException("Can not remove resource in use");
-        } catch(EmptyResultDataAccessException exception) {
-            throw new NotFoundException("Resource not found");
-        }
+    getRestaurant(id);
+     restaurantRepository.deleteById(id);
     }
 
+    private Restaurant getRestaurant(Long id) {
+        return restaurantRepository.findById(id).orElseThrow(() -> new NotFoundException("Resource not found"));
+    }
 }
