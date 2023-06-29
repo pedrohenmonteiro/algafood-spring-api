ab826dfbe33deaa18f8e7aaf4ab43d969819debf
Pedro Monteiro <pedromont436@gmail.com>
2023-06-28 23:18:53 -0300

    StateController class

---
+++ src/main/java/com/mont/algafoodapi/api/controller/StateController.java
@@ -0,0 +1,56 @@
+package com.mont.algafoodapi.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mont.algafoodapi.domain.model.State;
+import com.mont.algafoodapi.domain.service.StateService;
+
+@RestController
+@RequestMapping("/state")
+public class StateController {
+    
+
+
+    @Autowired
+    private StateService stateService;
+
+
+    @GetMapping
+    public ResponseEntity<List<State>> findAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(stateService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<State> findById(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(stateService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<State> create(@RequestBody State state) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(stateService.create(state));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<State> update(@PathVariable Long id, @RequestBody State state) {
+        state.setId(id);
+        return ResponseEntity.status(HttpStatus.OK).body(stateService.update(id, state));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        stateService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
