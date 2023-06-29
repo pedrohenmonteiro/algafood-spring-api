193f9fe86581908c6e1d99bb66a5c8e680227d51
Pedro Monteiro <pedromont436@gmail.com>
2023-06-29 18:06:47 -0300

    Refactoring StateService

--- src/main/java/com/mont/algafoodapi/domain/service/StateService.java
+++ src/main/java/com/mont/algafoodapi/domain/service/StateService.java
@@ -3,11 +3,8 @@ package com.mont.algafoodapi.domain.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
-import com.mont.algafoodapi.domain.exception.BadRequestException;
 import com.mont.algafoodapi.domain.exception.NotFoundException;
 import com.mont.algafoodapi.domain.model.State;
 import com.mont.algafoodapi.domain.repository.StateRepository;
@@ -15,17 +12,18 @@ import com.mont.algafoodapi.domain.repository.StateRepository;
 @Service
 public class StateService {
     
-    @Autowired
+    @Autowired 
     private StateRepository stateRepository;
 
     public List<State> findAll() {
         return stateRepository.findAll();
+        
     }
 
     public State findById(Long id) {
-        return stateRepository.findById(id);
+        return getState(id);
     }
-
+    
     public State create(State state) {
         return stateRepository.save(state);
     }
@@ -36,13 +34,11 @@ public class StateService {
     }
 
     public void delete(Long id) {
+    getState(id);
+     stateRepository.deleteById(id);
+    }
 
-    try {
-        stateRepository.delete(id);
-         } catch(DataIntegrityViolationException exception) {
-            throw new BadRequestException("Can not remove resource in use");
-        } catch(EmptyResultDataAccessException exception) {
-            throw new NotFoundException("Resource not found");
-        }
+    private State getState(Long id) {
+        return stateRepository.findById(id).orElseThrow(() -> new NotFoundException("Resource not found"));
     }
 }
