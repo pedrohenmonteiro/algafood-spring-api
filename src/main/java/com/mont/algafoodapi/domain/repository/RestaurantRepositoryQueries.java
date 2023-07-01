a2bc59b4fd5890cf19f2a8f39c7f1e8a065c5f5c
Pedro Monteiro <pedromont436@gmail.com>
2023-06-30 23:15:49 -0300

    RestaurantRepository customized

--- src/main/java/com/mont/algafoodapi/domain/repository/RestaurantRepositoryQueries.java
+++ src/main/java/com/mont/algafoodapi/domain/repository/RestaurantRepositoryQueries.java
@@ -3,13 +3,10 @@ package com.mont.algafoodapi.domain.repository;
 import java.math.BigDecimal;
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.mont.algafoodapi.domain.model.Restaurant;
 
-public interface RestaurantRepository extends JpaRepository<Restaurant, Long>, RestaurantRepositoryQueries{
+public interface RestaurantRepositoryQueries {
 
-    @Override
     List<Restaurant> findByNameAndFee(String name, BigDecimal minDeliveryFee, BigDecimal maxDeliveryFee);
- 
+
 }
--- src/main/java/com/mont/algafoodapi/infraestructure/repository/RestaurantRepositoryImpl.java
+++ src/main/java/com/mont/algafoodapi/infraestructure/repository/RestaurantRepositoryImpl.java
@@ -6,16 +6,18 @@ import java.util.List;
 import org.springframework.stereotype.Repository;
 
 import com.mont.algafoodapi.domain.model.Restaurant;
+import com.mont.algafoodapi.domain.repository.RestaurantRepositoryQueries;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 
 @Repository
-public class RestaurantRepositoryImpl {
+public class RestaurantRepositoryImpl implements RestaurantRepositoryQueries {
     
     @PersistenceContext
     private EntityManager em;
 
+    @Override
     public List<Restaurant> findByNameAndFee(String name, BigDecimal minDeliveryFee, BigDecimal maxDeliveryFee) {
 
         var jpql = "from Restaurant where name like :name "
