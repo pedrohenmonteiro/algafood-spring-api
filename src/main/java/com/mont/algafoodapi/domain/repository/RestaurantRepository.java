91dd6f66241a73c7753a8d2b5098fb1c51639bd2
Pedro Monteiro <pedromont436@gmail.com>
2023-07-03 17:34:45 -0300

    Implementing a customized Jpa Repository

--- src/main/java/com/mont/algafoodapi/domain/repository/RestaurantRepository.java
+++ src/main/java/com/mont/algafoodapi/domain/repository/RestaurantRepository.java
@@ -3,12 +3,11 @@ package com.mont.algafoodapi.domain.repository;
 import java.math.BigDecimal;
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.mont.algafoodapi.domain.model.Restaurant;
 
-public interface RestaurantRepository extends JpaRepository<Restaurant, Long>, RestaurantRepositoryQueries, JpaSpecificationExecutor<Restaurant>{
+public interface RestaurantRepository extends CustomJpaRepository<Restaurant, Long>, RestaurantRepositoryQueries, JpaSpecificationExecutor<Restaurant>{
 
     @Override
     List<Restaurant> findByNameAndFee(String name, BigDecimal minDeliveryFee, BigDecimal maxDeliveryFee);
