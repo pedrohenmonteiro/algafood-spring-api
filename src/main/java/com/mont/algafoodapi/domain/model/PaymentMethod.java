9cfa122dabbcb9ad5e7ceff4a26e4b0836dc0054
Pedro Monteiro <pedromont436@gmail.com>
2023-07-03 19:37:48 -0300

    PaymentMethod entity

--- src/main/java/com/mont/algafoodapi/domain/model/PaymentMethod.java
+++ src/main/java/com/mont/algafoodapi/domain/model/PaymentMethod.java
@@ -1,15 +1,9 @@
 package com.mont.algafoodapi.domain.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -18,18 +12,14 @@ import lombok.NoArgsConstructor;
 @Entity
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-@NoArgsConstructor
 @AllArgsConstructor
-public class Cuisine {
+@NoArgsConstructor
+
+public class PaymentMethod {
     
-    @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
-
-    @JsonIgnore
-    @OneToMany(mappedBy = "cuisine")
-    private List<Restaurant> restaurants = new ArrayList<>();
+    private String description;
 }
