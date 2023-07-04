fce2989ada3ad5d3d6a615e6d1da52e57e0aeea3
Pedro Monteiro <pedromont436@gmail.com>
2023-07-04 14:00:12 -0300

    Permission entity

---
+++ src/main/java/com/mont/algafoodapi/domain/model/Permission.java
@@ -0,0 +1,23 @@
+package com.mont.algafoodapi.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Permission {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+}
