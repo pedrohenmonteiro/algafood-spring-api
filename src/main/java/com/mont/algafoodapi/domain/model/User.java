f39ebcc346fc642193bcdff409d3c2db6bfb5741
Pedro Monteiro <pedromont436@gmail.com>
2023-07-04 15:32:26 -0300

    User entity

---
+++ src/main/java/com/mont/algafoodapi/domain/model/User.java
@@ -0,0 +1,36 @@
+package com.mont.algafoodapi.domain.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+
+    @JsonIgnore
+    @CreationTimestamp
+    private LocalDateTime creationDate;
+}
