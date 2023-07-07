33b8876a74f0ab2ca3393d5127d61e2052f402f0
Pedro Monteiro <pedromont436@gmail.com>
2023-07-06 23:22:49 -0300

    Implementing a default ReponseEntityExpetionHandler exceiptions

--- src/main/java/com/mont/algafoodapi/domain/exception/handler/ResourceHandler.java
+++ src/main/java/com/mont/algafoodapi/domain/exception/handler/ResourceHandler.java
@@ -2,11 +2,14 @@ package com.mont.algafoodapi.domain.exception.handler;
 
 import java.time.Instant;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.mont.algafoodapi.domain.exception.BadRequestException;
 import com.mont.algafoodapi.domain.exception.ConflictException;
@@ -14,7 +17,7 @@ import com.mont.algafoodapi.domain.exception.ExceptionResponse;
 import com.mont.algafoodapi.domain.exception.NotFoundException;
 
 @RestControllerAdvice
-public class ResourceHandler {
+public class ResourceHandler extends ResponseEntityExceptionHandler{
     
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ExceptionResponse> notFoundExceptionHandler(Exception ex, WebRequest req)
@@ -35,6 +38,17 @@ public class ResourceHandler {
     {
         var exceptionResponse = new ExceptionResponse(Instant.now(), ex.getMessage(), req.getDescription(false), HttpStatus.CONFLICT.value());
         return ResponseEntity.status(HttpStatus.CONFLICT).body(exceptionResponse);
+    }
+
+
+    // default implementation for ResponseEntityExceptionHandler
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+            HttpStatusCode statusCode, WebRequest req) {
+
+        body = new ExceptionResponse(Instant.now(), ex.getMessage(), req.getDescription(false), statusCode.value());
+
+        return super.handleExceptionInternal(ex, body, headers, statusCode, req);
     }
 }
 
