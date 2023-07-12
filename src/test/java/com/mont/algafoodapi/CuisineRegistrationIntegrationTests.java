5cc236f735e83250400c3a0d7413469d7e2433fb
Pedro Monteiro <pedromont436@gmail.com>
2023-07-12 00:32:26 -0300

    Creating more tests for cuisine

--- src/test/java/com/mont/algafoodapi/CuisineRegistrationIntegrationTests.java
+++ src/test/java/com/mont/algafoodapi/CuisineRegistrationIntegrationTests.java
@@ -3,12 +3,18 @@ package com.mont.algafoodapi;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 
+import com.mont.algafoodapi.domain.exception.ConflictException;
+import com.mont.algafoodapi.domain.exception.NotFoundException;
 import com.mont.algafoodapi.domain.model.Cuisine;
+import com.mont.algafoodapi.domain.model.Restaurant;
 import com.mont.algafoodapi.domain.service.CuisineService;
 
 import jakarta.validation.ConstraintViolationException;
@@ -20,7 +26,7 @@ class CuisineRegistrationIntegrationTests {
 		private CuisineService cuisineService;
 
 	@Test
-	void cuisineRegistrationWithSuccess() {
+	void mustAssignId_when_registrateCuisine() {
 		//scenary
 		Cuisine newCuisine = new Cuisine();
 		newCuisine.setName("Chinese");
@@ -40,6 +46,21 @@ class CuisineRegistrationIntegrationTests {
 		
 		assertThrows(ConstraintViolationException.class, () -> {
 			cuisineService.create(newCuisine);
+		});
+	}
+
+	@Test
+	void mustFail_when_deleteCuisineAlreadyInUse() {
+		assertThrows(ConflictException.class, () -> {
+			cuisineService.delete(1L);
+		});
+	}
+
+	@Test
+	void mustFail_when_deleteInexistentCuisine() {
+
+	assertThrows(NotFoundException.class, () -> {
+			cuisineService.delete(100L);
 		});
 	}
 
