1a300b9cb86ee7e7facf4dc1f4773a5c1a4ac5a6
Pedro Monteiro <pedromont436@gmail.com>
2023-07-19 23:26:14 -0300

    CityMapper class

--- src/main/java/com/mont/algafoodapi/api/mapper/CityMapper.java
+++ src/main/java/com/mont/algafoodapi/api/mapper/CityMapper.java
@@ -1,8 +1,11 @@
 package com.mont.algafoodapi.api.mapper;
 
+import java.util.List;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
+import com.mont.algafoodapi.api.model.CityDto;
 import com.mont.algafoodapi.api.model.input.CityInputDto;
 import com.mont.algafoodapi.domain.model.City;
 
@@ -16,6 +19,11 @@ public class CityMapper {
         return modelMapper.map(cityInput, City.class);
     }
 
-    
+    public CityDto fromEntityToDto(City city) {
+        return modelMapper.map(city, CityDto.class);
+    }
 
-}
+    public List<CityDto> toCollectionDto(List<City> cities) {
+        return cities.stream().map(this::fromEntityToDto).toList();
+    }
+} 
