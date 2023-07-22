f6856a001ee7854ec11bd2f9f8e28da3749cbdfe
Pedro Monteiro <pedromont436@gmail.com>
2023-07-22 00:44:36 -0300

    UserMapper class

---
+++ src/main/java/com/mont/algafoodapi/api/mapper/UserMapper.java
@@ -0,0 +1,34 @@
+package com.mont.algafoodapi.api.mapper;
+
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.mont.algafoodapi.api.model.UserDto;
+import com.mont.algafoodapi.api.model.input.UserInputDto;
+import com.mont.algafoodapi.domain.model.User;
+
+@Component
+public class UserMapper {
+    
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public UserDto fromEntityToDto(User user) {
+        return modelMapper.map(user, UserDto.class);
+    }
+    
+    public User fromDtoToEntity(UserInputDto userInputDto) {
+        return modelMapper.map(userInputDto, User.class);
+    }
+
+    public List<UserDto> toCollectionDto(List<User> user) {
+      return user.stream().map(this::fromEntityToDto).toList();
+    }
+
+    public void copyToDomainObject(UserInputDto userInputDto, User user) {
+        modelMapper.map(userInputDto, user);
+    }
+}
