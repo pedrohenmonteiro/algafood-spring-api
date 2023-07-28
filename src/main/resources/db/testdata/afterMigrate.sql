08b0a8f679239b1cdab037ebe5871271cf46c2f7
Pedro Monteiro <pedromont436@gmail.com>
2023-07-28 16:15:38 -0300

    creating Permission dtos

--- src/main/resources/db/testdata/afterMigrate.sql
+++ src/main/resources/db/testdata/afterMigrate.sql
@@ -89,3 +89,5 @@ insert into user (id, name, email, password, creation_date) values
 (2, 'Maria Joaquina', 'maria.vnd@algafood.com', '123', utc_timestamp),
 (3, 'José Souza', 'jose.aux@algafood.com', '123', utc_timestamp),
 (4, 'Sebastião Martins', 'sebastiao.cad@algafood.com', '123', utc_timestamp);      
+
+insert into group_permission (tb_group_id, permission_id) values (1, 1), (1, 2), (2, 1), (2, 2), (3, 1); 
