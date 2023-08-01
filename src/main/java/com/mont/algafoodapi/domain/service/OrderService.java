58b91df5a3bdbf916808b3bb4b7f9b95d2c3ea3d
Pedro Monteiro <pedromont436@gmail.com>
2023-08-01 00:53:05 -0300

    OrderService class

---
+++ src/main/java/com/mont/algafoodapi/domain/service/OrderService.java
@@ -0,0 +1,35 @@
+package com.mont.algafoodapi.domain.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mont.algafoodapi.api.mapper.OrderMapper;
+import com.mont.algafoodapi.api.model.OrderDto;
+import com.mont.algafoodapi.domain.exception.NotFoundException;
+import com.mont.algafoodapi.domain.model.Order;
+import com.mont.algafoodapi.domain.repository.OrderRepository;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private OrderMapper orderMapper;
+    
+    public List<OrderDto> findAll() {
+        return orderMapper.toCollectionDto(orderRepository.findAll());
+    }
+
+    public OrderDto findById(Long id) {
+        var order = getOrder(id);
+        return orderMapper.fromEntityToDto(order);
+    }
+
+    protected Order getOrder(Long id) {
+        return orderRepository.findById(id).orElseThrow(() -> new NotFoundException("Resource order id " + id + " not found"));
+    }
+}
