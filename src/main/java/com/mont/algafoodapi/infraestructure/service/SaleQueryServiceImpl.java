d2080fdb26c2bcaaf8adfbbaae03f305beae203d
Pedro Monteiro <pedromont436@gmail.com>
2023-08-10 23:15:04 -0300

    implementing filters for daily sales

--- src/main/java/com/mont/algafoodapi/infraestructure/service/SaleQueryServiceImpl.java
+++ src/main/java/com/mont/algafoodapi/infraestructure/service/SaleQueryServiceImpl.java
@@ -1,17 +1,20 @@
 package com.mont.algafoodapi.infraestructure.service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
 import com.mont.algafoodapi.domain.filter.DailySaleFilter;
 import com.mont.algafoodapi.domain.model.Order;
+import com.mont.algafoodapi.domain.model.OrderStatus;
 import com.mont.algafoodapi.domain.model.dto.DailySale;
 import com.mont.algafoodapi.domain.service.SaleQueryService;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.criteria.Predicate;
 
 @Repository
 public class SaleQueryServiceImpl implements SaleQueryService {
@@ -25,6 +28,7 @@ public class SaleQueryServiceImpl implements SaleQueryService {
         var query = builder.createQuery(DailySale.class);
         var root = query.from(Order.class);
 
+        List<Predicate> predicates = new ArrayList<>();
 
         var functionDateCreation = builder.function("date", LocalDate.class, root.get("creationDate"));
 
@@ -34,7 +38,23 @@ public class SaleQueryServiceImpl implements SaleQueryService {
             builder.sum(root.get("totalValue"))
         );
 
+        if(filter.getRestaurantId() != null) {
+            predicates.add(builder.equal(root.get("restaurant").get("id"), filter.getRestaurantId()));
+        }
+
+        if(filter.getCreationDateInit() != null) {
+            predicates.add(builder.greaterThanOrEqualTo(root.get("creationDate"), filter.getCreationDateInit()));
+        }
+
+        if(filter.getCreationDateEnd() != null) {
+            predicates.add(builder.lessThanOrEqualTo(root.get("creationDate"), filter.getCreationDateEnd()));
+        }
+
+        predicates.add(root.get("status").in(OrderStatus.CONFIRMED, OrderStatus.DELIVERED));
+        
+
         query.select(selection);
+        query.where(predicates.toArray(new Predicate[0]));
         query.groupBy(functionDateCreation);
 
         return em.createQuery(query).getResultList();
