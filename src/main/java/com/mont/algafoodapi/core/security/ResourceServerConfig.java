dcddbefa257d4144603a92e5dcfe8c463ceb01f0
Pedro Monteiro <pedromont436@gmail.com>
2023-09-27 22:28:16 -0300

    loading GrantedAuthorities and restricting access to endpoints

--- src/main/java/com/mont/algafoodapi/core/security/ResourceServerConfig.java
+++ src/main/java/com/mont/algafoodapi/core/security/ResourceServerConfig.java
@@ -1,9 +1,19 @@
 package com.mont.algafoodapi.core.security;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,10 +22,38 @@ public class ResourceServerConfig {
     @Bean
     SecurityFilterChain resourceServerFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((authorize) -> authorize
-        .anyRequest().authenticated())
-        .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()));
+            .requestMatchers(HttpMethod.POST, "/cuisine/**").hasAuthority("EDIT_CUISINE")
+            .requestMatchers(HttpMethod.PUT, "/cuisine/**").hasAuthority("EDIT_CUISINE")
+            .requestMatchers(HttpMethod.GET, "/cuisine/**").authenticated()
+            .anyRequest().denyAll())
+        .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()).jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())));
 
 
         return http.build();
     } 
+
+
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            List<String> authorities = jwt.getClaimAsStringList("authorities");
+
+            if (authorities == null) {
+                return Collections.emptyList();
+            }
+
+            JwtGrantedAuthoritiesConverter authoritiesConverter = new JwtGrantedAuthoritiesConverter();
+            Collection<GrantedAuthority> grantedAuthorities = authoritiesConverter.convert(jwt);
+
+            grantedAuthorities.addAll(authorities
+                    .stream()
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toList()));
+
+            return grantedAuthorities;
+        });
+
+        return converter;
+    }
 }
